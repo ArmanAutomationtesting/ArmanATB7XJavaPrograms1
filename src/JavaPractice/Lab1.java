ed50ae1e3bd13c4a5825b21fa35838665017c03e
ahmadarman532@gmail.com <ahmadarman532@gmail.com>
2024-07-28 15:09:31 +0530

    2nd commit

--- src/JavaPractice/Lab1.java
+++ src/JavaPractice/Lab1.java
@@ -1,7 +1,18 @@
 package JavaPractice;
 
 public class Lab1 {
+    String name;
+    String father_Name;
+    int age;
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        Lab1 Profile = new Lab1();
+        Profile.name = "Arman";
+        Profile.father_Name = "Tauheed Ahmad";
+        Profile.age= 25;
+        System.out.println(Profile.age);
+        System.out.println("Father Name is = " + Profile.father_Name);
+
+
     }
 }
